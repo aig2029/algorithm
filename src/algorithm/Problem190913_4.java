ec37375d850bee6a305a80da752512e58019ab2b
이승진 <aig2029@naver.com>
2019-09-13 20:08:30 +0900

    algo0808

--- src/algorithm/Problem190913_4.java
+++ src/algorithm/Problem190913_4.java
@@ -1,11 +1,10 @@
 package algorithm;
 
-public class Problem190913_0 {
+public class Problem190913_4 {
 
-	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		System.out.print("\'Hello\'");
 	}
 
 }
