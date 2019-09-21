24985415341e29d7c7653c4d26eb3339ef33a9be
이승진 <aig2029@naver.com>
2019-09-21 11:12:29 +0900

    code up 문제집 : 기초 100제

--- src/algorithm/Problem190921_3.java
+++ src/algorithm/Problem190921_3.java
@@ -1,10 +1,15 @@
 package algorithm;
 
+import java.util.Scanner;
+
 public class Problem190921_3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("zzㅋaz");
+		Scanner sc = new Scanner(System.in);
+		float ft = sc.nextFloat();
+		String s = String.format("%.6f", ft);
+		System.out.print(s);
 	}
 
 }
---
+++ src/algorithm/Problem190921_4.java
@@ -0,0 +1,10 @@
+package algorithm;
+
+public class Problem190921_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
