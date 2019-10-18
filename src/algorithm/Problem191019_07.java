d12390b449552ab92dc2c5ae1f7d732feb1d2418
이승진 <aig2029@naver.com>
2019-10-19 08:43:13 +0900

    프로그래머스 - 자연수 뒤집어 배열로 만들기
    https://programmers.co.kr/learn/courses/30/lessons/12932#

--- src/algorithm/Problem191019_07.java
+++ src/algorithm/Problem191019_07.java
@@ -7,18 +7,25 @@ public class Problem191019_07 {
 		
 	}
 	
-	public int solution(int n) {
-        int answer = 0;
+	public int[] solution(long n) {
+		int[] answer;
 
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
-        
-        while(n!=0){
-            answer+=(n%10);
-            n/=10;
-        }
+		System.out.println((int) (Math.log10(n) + 1));
+		long size = (long) (Math.log10(n) + 1);
+		answer = new int[(int) size];
 
-        return answer;
-    }
+		int idx = 0;
+		while (n != 0) {
+			int res = (int) (n % 10);
+			answer[idx++] = res;
+			n /= 10;
+		}
+
+		for (int i = 0; i < answer.length; i++) {
+			System.out.println(answer[i]);
+		}
+
+		return answer;
+	}
 	
 }
