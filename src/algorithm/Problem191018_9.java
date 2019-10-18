3de75019ebaa28eb09680fb3055c250bfeca0f7a
이승진 <aig2029@naver.com>
2019-10-18 19:51:33 +0900

    프로그래머스 - 서울에서 김서방 찾기
    https://programmers.co.kr/learn/courses/30/lessons/12919

--- src/algorithm/Problem191018_9.java
+++ src/algorithm/Problem191018_9.java
@@ -8,8 +8,12 @@ public class Problem191018_9 {
 	}
 	
 	public String solution(String[] seoul) {
-		String answer = "";
-		return answer;
+		for (int i = 0; i < seoul.length; i++) {
+			if(seoul[i].equals("Kim")) {
+				return "김서방은 "+i+"에 있다";
+			}
+		}
+		return "";
 	}
 
 }
