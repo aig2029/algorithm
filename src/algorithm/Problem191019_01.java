1592eceeff035593c7e48c74d060589e93d62302
이승진 <aig2029@naver.com>
2019-10-18 19:49:53 +0900

    프로그래머스 - 소수 찾기
    https://programmers.co.kr/learn/courses/30/lessons/12921

---
+++ src/algorithm/Problem191019_01.java
@@ -0,0 +1,29 @@
+package algorithm;
+
+import java.util.Arrays;
+
+public class Problem191019_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	 public int solution(int n) {
+	      int answer = 0;
+	      boolean[] sosu = new boolean[n+1];
+	      Arrays.fill(sosu,true);
+	      for(int i=2; i<=n; i++){
+	          if(sosu[i]==true){
+	              for(int j=2;j*i<=n;j++){
+	                  sosu[i*j]=false;
+	              }    
+	          }
+	      }
+	      for(int i=2;i<=n;i++){
+	          if(sosu[i]) answer++;
+	      }
+	      return answer;
+	  }
+
+}
