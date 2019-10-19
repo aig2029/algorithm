93ff009b3d5df4a385846b9becc5223d843d6ca2
이승진 <aig2029@naver.com>
2019-10-19 18:24:11 +0900

    프로그래머스 - 짝수와 홀수
    https://programmers.co.kr/learn/courses/30/lessons/12937

---
+++ src/algorithm/Problem191019_11.java
@@ -0,0 +1,17 @@
+package algorithm;
+
+public class Problem191019_11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public String solution(int num) {
+		if (num % 2 == 0)
+			return "Even";
+		else
+			return "Odd";//
+	}
+	
+}
