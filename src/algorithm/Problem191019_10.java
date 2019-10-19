fd5868277e1d860769093b299678a0ede88de3ef
이승진 <aig2029@naver.com>
2019-10-20 04:14:26 +0900

    프로그래머스 - 제일 작은 수 제거하기
    https://programmers.co.kr/learn/courses/30/lessons/12935

--- src/algorithm/Problem191019_10.java
+++ src/algorithm/Problem191019_10.java
@@ -7,4 +7,27 @@ public class Problem191019_10 {
 		
 	}
 	
+	public int[] solution(int[] arr) {
+		if (arr.length == 1)
+			return new int[] { -1 };
+
+		int[] answer = new int[arr.length - 1];
+
+		int min = arr[0];
+		for (int i = 1; i < arr.length; i++) {
+			if (min > arr[i])
+				min = arr[i];
+		}
+
+		int j = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == min)
+				continue;
+			else
+				answer[j++] = arr[i];
+		}
+
+		return answer;
+	}
+	
 }
