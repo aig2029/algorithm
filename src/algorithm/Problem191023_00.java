599e992ab87b1847b5ad391012ce4e20ef0747d2
이승진 <aig2029@naver.com>
2019-10-23 10:25:29 +0900

    프로그래머스 - 124 나라의 숫자
    https://programmers.co.kr/learn/courses/30/lessons/12899#

---
+++ src/algorithm/Problem191023_00.java
@@ -0,0 +1,29 @@
+package algorithm;
+
+public class Problem191023_00 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public String solution(int n) {
+		String answer = "";
+		int rest = 0;
+
+		while (n > 0) {
+			rest = n % 3;
+			n /= 3;
+			if (rest == 0) {
+				rest = 4;
+				n -= 1;
+			}
+			answer = rest + answer;
+		}
+
+		System.out.println(answer);
+
+		return answer;
+	}
+	
+}
