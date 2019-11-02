e772e33f5c2c00dd0c8dd1e605f7546bee2358f2
이승진 <aig2029@naver.com>
2019-11-02 19:27:44 +0900

    코드업

---
+++ src/algorithm/Problem191102_01.java
@@ -0,0 +1,17 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Problem191102_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		System.out.println(N + " " +N+ " "+N + " ");
+		
+	}
+
+	
+}
