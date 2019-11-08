90b7edf8155a62ea3e8c15c420977aa9ba151b3d
이승진 <aig2029@naver.com>
2019-11-08 21:12:48 +0900

    프로그래머스 - 행렬의 곱셈 (쉬움)
    https://programmers.co.kr/learn/courses/30/lessons/12949

---
+++ src/algorithm/Problem191108_02.java
@@ -0,0 +1,24 @@
+package algorithm;
+
+public class Problem191108_02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr2[0].length];
+        for(int i=0; i<answer.length;i++){
+            for(int j=0; j<answer[0].length; j++){
+                for(int k=0; k<arr2.length; k++){
+                    answer[i][j]+= arr1[i][k]*arr2[k][j];    
+                }
+            }
+        }
+ 
+        return answer;
+    }
+
+	
+}
