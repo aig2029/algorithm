d48d807fb43b9a3e5f6b937b7d600462bd097b56
이승진 <aig2029@naver.com>
2019-11-16 00:05:18 +0900

    프로그래머스 - [1차] 추석 트래픽 (테케3,18뺴고성공)
    https://programmers.co.kr/learn/courses/30/lessons/17676

--- src/algorithm/Problem191113_08.java
+++ src/algorithm/Problem191113_08.java
@@ -8,7 +8,7 @@ public class Problem191113_08 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-	}
+	}//
 	
 	public int solution(String[] lines) {
 		int answer = 0;
