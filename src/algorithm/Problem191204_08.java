294c211dd9db005a2688dd50a4171554c52c9f84
이승진 <aig2029@naver.com>
2019-12-04 11:53:46 +0900

    프로그래머스 - 디스크 컨트롤러 (힙)
    https://programmers.co.kr/learn/courses/30/lessons/42627#
    
    테케 8,18 fail 했었음.
    맨처음 Arrays.sort할 때 작업 요청시간을 기준으로만 정렬해줫는데
    작업요청시간이 동일한 경우를 간과했음. 작업요청시간이 동일한 작업들은 소요시간을 기준으로 오름차순으로 정렬해줘야함

---
+++ src/algorithm/Problem191204_08.java
@@ -0,0 +1,103 @@
+package algorithm;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Problem191204_08 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public int solution(int[][] jobs) {
+		int answer = 0;
+
+		Arrays.sort(jobs, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] j1, int[] j2) {
+				if (j1[0] < j2[0])
+					return -1;
+				else if (j1[0] == j2[0]) {
+					if (j1[1] < j2[1])
+						return -1;
+				}
+				return 1;
+			}
+		});
+
+		// for(int i=0; i<jobs.length; i++){
+		//     System.out.println(jobs[i][0]+" "+jobs[i][1]);
+		// }
+
+		PriorityQueue<Job> waitList = new PriorityQueue<>();
+		Queue<Job> q = new LinkedList<>();
+
+		for (int i = 0; i < jobs.length; i++) {
+			q.add(new Job(jobs[i][0], jobs[i][1]));
+		}
+
+		// System.out.println();
+		// while(!q.isEmpty()){
+		//     System.out.println(q.poll());
+		// }
+
+		if (jobs.length == 1)
+			return jobs[0][1];
+
+		int time = 0;
+		Job now = null;
+
+		while (true) {
+			if (now == null) {
+				if (waitList.isEmpty()) {
+					now = q.poll();
+					time = now.start;
+				} else {
+					now = waitList.poll();
+				}
+			} else {
+				time += now.workTime;
+				answer += (time - now.start);
+				now = null;
+
+				while (!q.isEmpty() && q.peek().start <= time) {
+					waitList.add(q.poll());
+				}
+
+			}
+
+			if (waitList.isEmpty() && q.isEmpty() && now == null)
+				break;
+		}
+
+		answer /= jobs.length;
+
+		return answer;
+	}
+
+	class Job implements Comparable<Job> {
+		int start;
+		int workTime;
+
+		Job(int start, int workTime) {
+			this.start = start;
+			this.workTime = workTime;
+		}
+
+		@Override
+		public int compareTo(Job j) {
+			return Integer.compare(this.workTime, j.workTime);
+		}
+
+		public String toString() {
+			return this.start + " " + this.workTime;
+		}
+
+	}
+
+	
+}
