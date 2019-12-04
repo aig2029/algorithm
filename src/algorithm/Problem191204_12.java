2425c0d3552fac71eb1d76089429cbace993447d
이승진 <aig2029@naver.com>
2019-12-04 17:26:17 +0900

    프로그래머스 - SQL (SUM, MAX, MIN)
    https://programmers.co.kr/learn/courses/30/parts/17043

---
+++ src/algorithm/Problem191204_12.java
@@ -0,0 +1,28 @@
+package algorithm;
+
+public class Problem191204_12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+//	최대값 구하기
+//	SELECT datetime as 시간
+//	FROM animal_ins
+//	WHERE datetime = ( SELECT MAX(datetime) FROM animal_ins )
+//	
+//	최소값 구하기
+//	SELECT datetime as 시간
+//	FROM animal_ins
+//	WHERE datetime = ( SELECT MIN(datetime) FROM animal_ins )
+//	
+//	동물 수 구하기
+//	SELECT count(*) as count
+//	FROM animal_ins
+//	
+//	중복 제거하기
+//	SELECT count(DISTINCT name)  as count
+//	FROM animal_ins
+//	WHERE NOT name is NULL
+}
