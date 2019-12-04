8f00332e5efe6cc6dcd859c463595426ca0306b9
이승진 <aig2029@naver.com>
2019-12-04 17:09:57 +0900

    프로그래머스 - SQL (SELECT)
    https://programmers.co.kr/learn/courses/30/parts/17042

---
+++ src/algorithm/Problem191204_11.java
@@ -0,0 +1,50 @@
+package algorithm;
+
+public class Problem191204_11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+//	모든 레코드 조회하기
+//	SELECT * 
+//	FROM ANIMAL_INS
+//	ORDER BY ANIMAL_ID DESC
+//	
+//	역순 정렬하기
+//	SELECT NAME, DATETIME
+//	FROM ANIMAL_INS
+//	ORDER BY ANIMAL_ID DESC
+//	
+//	아픈 동물 찾기
+//	SELECT ANIMAL_ID, NAME
+//	FROM ANIMAL_INS
+//	WHERE INTAKE_CONDITION='Sick'
+//	ORDER BY ANIMAL_ID
+//	
+//	어린 동물 찾기
+//	SELECT ANIMAL_ID, NAME
+//	FROM ANIMAL_INS
+//	WHERE INTAKE_CONDITION!='Aged'
+//	ORDER BY ANIMAL_ID
+//	
+//	동물의 아이디와 이름
+//	SELECT ANIMAL_ID, NAME
+//	FROM ANIMAL_INS
+//	ORDER BY ANIMAL_ID
+//	
+//	여러 기준으로 정렬하기
+//	SELECT animal_id, name, datetime
+//	FROM animal_ins
+//	ORDER BY name, datetime desc
+//	
+//	
+//	상위 n개 레코드
+//	SELECT name
+//	FROM animal_ins
+//	WHERE DATETIME=(SELECT MIN(DATETIME) FROM animal_ins)
+//	
+	
+	
+}
