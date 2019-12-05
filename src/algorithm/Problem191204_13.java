268bc1c465d047792f3b724171890ab5f9aa964e
이승진 <aig2029@naver.com>
2019-12-05 14:05:03 +0900

    프로그래머스 - SQL (GROUP BY)
    https://programmers.co.kr/learn/courses/30/parts/17044
    
    마지막문제안풀엇음

---
+++ src/algorithm/Problem191204_13.java
@@ -0,0 +1,35 @@
+package algorithm;
+
+public class Problem191204_13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+//	고양이와 개는 몇 마리 있을까
+//	SELECT animal_type, count(datetime)
+//	FROM animal_ins
+//	GROUP BY animal_type
+//	ORDER BY animal_type
+//	
+//	동명 동물 수 찾기
+//	SELECT name, count(name)
+//	FROM animal_ins
+//	GROUP BY name
+//	HAVING count(name)>1
+//	ORDER BY name
+//	
+//	입양 시각 구하기(1)
+//	SELECT hour, count(*)
+//	FROM
+//	    (SELECT to_char(datetime,'HH24') as hour
+//	    FROM animal_outs)
+//	WHERE hour>=9 and hour<=19
+//	GROUP BY hour
+//	ORDER BY hour
+
+	
+	
+	
+}
