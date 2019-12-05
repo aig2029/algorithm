865043390c3d950baeb95aa15cb69026b5f23b94
이승진 <aig2029@naver.com>
2019-12-05 14:25:32 +0900

    프로그래머스 - SQL (IS NULL)
    https://programmers.co.kr/learn/courses/30/parts/17045

---
+++ src/algorithm/Problem191205_01.java
@@ -0,0 +1,29 @@
+package algorithm;
+
+public class Problem191205_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+//	이름이 없는 동물의 아이디
+//	SELECT animal_id
+//	FROM animal_ins
+//	WHERE name is NULL
+//	ORDER BY animal_id
+//	
+//	이름이 있는 동물의 아이디
+//	SELECT animal_id
+//	FROM animal_ins
+//	WHERE name is not NULL
+//	ORDER BY animal_id
+//	
+//	Null 처리하기
+//	-- SELECT NVL(칼럼명, 0)
+//	SELECT animal_type, NVL(name,'No name') as NAME, sex_upon_intake
+//	FROM animal_ins
+//	ORDER BY animal_id
+
+	
+}
