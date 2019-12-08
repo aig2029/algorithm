8c2d1985e46efd649a846202d89e0bb1d91d822d
이승진 <aig2029@naver.com>
2019-12-08 21:06:05 +0900

    프로그래머스 - 징검다리 (H-Index)
    https://programmers.co.kr/learn/courses/30/lessons/42747
    
    별로 좋아하는 문제는 아님..

--- src/algorithm/Problem191207_03.java
+++ src/algorithm/Problem191207_03.java
@@ -1,10 +1,35 @@
 package algorithm;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Problem191207_03 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	public int solution(int[] citationsInput) {
+		int answer = 0;
+
+		ArrayList<Integer> citations = new ArrayList<>();
+		for (int c : citationsInput) {
+			citations.add(c);
+		}
+
+		Collections.sort(citations);
+		Collections.reverse(citations);
+
+		int accum = 0;
+		for (int c : citations) {
+			if (accum >= c)
+				return accum;
+			accum++;
+			// System.out.println(c);
+		}
+
+		return accum;
 	}
 
 	
