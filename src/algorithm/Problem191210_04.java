3467c60008e46c9907ed1950df0390632034ef01
이승진 <aig2029@naver.com>
2019-12-10 12:56:35 +0900

    codility - Time Complexity (PermCheck)

---
+++ src/algorithm/Problem191210_04.java
@@ -0,0 +1,24 @@
+package algorithm;
+
+import java.util.Arrays;
+
+public class Problem191210_04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int solution(int[] A) {
+
+		Arrays.sort(A);
+		int comparativeValue = 1;
+		for (int i = 0; i < A.length; i++) {
+			if (comparativeValue++ != A[i])
+				return 0;
+
+		}
+
+		return 1;
+	}
+}
